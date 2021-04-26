991999709455a388eeea75cd2a9f64a8afd9a871
Dazai26672 <captainskynom@gmail.com>
2021-04-26 21:36:08 +0200

    Added file reading code from deepnote and implemented progress bar

--- main.py
+++ main.py
@@ -1,2 +1,38 @@
 # main program
-print("hello world")
+import os
+import json
+import pandas as pd
+import numpy as np
+
+
+
+directory = os.fsencode(r"D:\data")
+dfx = pd.DataFrame([])
+
+total_files = len([name for name in os.listdir('D:\data')])
+counter = 0
+
+for file in os.listdir(directory):
+    filename = os.fsdecode(file)
+    if filename.endswith(".json"):
+        counter += 1
+        tweetlist = []
+        with open(r'D:\data\{}'.format(filename)) as f:
+            for line in f:
+                if 'Exceeded connection limit for user' in line:
+                    continue
+                else:
+                    tweetDict = json.loads(line)
+                    tweetlist.append(tweetDict)
+
+            dfx = dfx.append(pd.DataFrame(tweetlist))
+
+            progress = (counter / total_files) * 100
+            print(f"{round(progress, 2)}%")
+        continue
+
+
+    else:
+        continue
+
+print(len(tweetlist))
--- test.py
+++ test.py
@@ -1,1 +1,8 @@
-print('Test')
+import os
+
+counter = 300
+
+total_files = len([name for name in os.listdir('D:\data')])
+progress = (counter/total_files) * 100
+
+print(f"{round(progress, 2)}%")
